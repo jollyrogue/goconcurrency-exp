7b1b15c4ed6093a22fa8c20cb0a0cab583265d71
Ryan Quinn <quinn.rm@gmail.com>
2019-02-01 16:04:59 -0600

    Change the name of runLoop to runSignal since it doesn't use a loop anymore.
     Changes to be committed:
    	renamed:    runLoop.go -> runSignal.go

--- runSignal.go
+++ runSignal.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 	"time"
+	"os/signal"
+	"syscall"
+	"os"
 )
 
 func main() {
+	// Creating a channel to watch for signals.
+	sig := make(chan os.Signal)
+	// Channel to signal the goroutines to stop.
 	work := make(chan bool)
+
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
 	hostInterval := 15 * time.Second
 	serviceInterval := 5 * time.Second
-	runtime := 60 * time.Second
+
 
 	// Running each function manually since we want the checks to fire off without waiting the first time. After the first time, the checks will fire off on schedule.
 	log.Println("------ First Checks Start ------")
@@ -21,7 +29,10 @@ func main() {
 	log.Println("------ Scheduled Checks Start ------")
 	host := schedule(hostAlive, hostInterval, work)
 	service := schedule(serviceStatus, serviceInterval, work)
-	time.Sleep(runtime)
+
+	// Waiting for signal to finish executing the rest of the code.
+	flag := <-sig
+	log.Printf("Caught %s signal. Stopping loop...", flag)
 	close(work)
 	log.Println("------ Scheduled Checks End ------")
 	host.Stop()
